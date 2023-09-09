88b9884e72e15e82dc7e70c345289437253b03d1
Menna <mennaelhosiny>
2023-09-10 02:18:38 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = () => {
+  const url = "https://api.quotable.io/random";
+  let quoteData = {
+    content: "Let time be your only competitor.",
+    author: "Ahmed Saber"
+  }
+  const [quote, setQuote] = useState(quoteData)
+
+  const generateQuote = () => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setQuote(data)
+      });
+  }
+
+  const copy = () => {
+    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
+    alert('copied')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Quote Generator React App</h1>
+      <div className="container">
+        <p>{quote.content}</p>
+        <span>{quote.author}</span>
+        <div className="btns">
+          <button onClick={copy} className="btn">Copy</button>
+          <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+      </div>
+    </>
+  )
 }
+
 
 export default App;
